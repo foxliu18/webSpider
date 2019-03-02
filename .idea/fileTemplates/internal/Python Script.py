6c7ce464eae6c627697b4a2b436be236a31c25d2
lkx-rog <foxliu18@hotmail.com>
2019-03-02 14:57:04 +0800

    remove .idea

--- .idea/fileTemplates/internal/Python Script.py
+++
@@ -1,7 +0,0 @@
-##!/usr/bin/python3.7
-# -*- coding: utf-8 -*-
-# @Time    : ${DATE} ${TIME}
-# @Author  : ${USER}
-# @Project : ${PROJECT_NAME}
-# @File    : ${NAME}.py
-# @Software: ${PRODUCT_NAME}
